220dd5b01c1dfd73ff1f9a365483386bb812eb63
packkrittaphat <krittaphatkmitl@gmail.com>
2021-08-02 19:33:22 +0700

    Add redux-logger

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,13 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import taskReducer from "../features/taskListSlice";
+import reduxLogger from "redux-logger";
 
 export const store = configureStore({
   reducer: {
     task: taskReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(reduxLogger),
 });
 
 export type AppDispatch = typeof store.dispatch;
